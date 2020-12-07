9991a130685a038621a9a5ab0742e2ac03fc4d5d
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-12-07 12:06:47 +0200

    refactor to dinamically change board size

--- exercises/program/chessboard.js
+++ exercises/program/chessboard.js
@@ -8,8 +8,10 @@ for (let i = 0; i < 8; i++) {
 
 console.log("\n--- Second option ---");
 let board = "";
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
+let size = 8;
+
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
     if ((i + j) % 2 == 0) {
       board += " ";
     } else {
