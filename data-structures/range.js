a4bb4554193dbf65c3eb2234184f21e17fb2e369
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-03-26 10:57:48 +0200

    print ranges

--- data-structures/range.js
+++ data-structures/range.js
@@ -8,6 +8,8 @@ function range(start, end, step = 1) {
     for (let value = start; end <= value; value += step) {
       result.push(value);
     }
+  } else {
+    result.push(start)
   }
 
   return result;
@@ -22,6 +24,8 @@ function sum(values) {
   return total;
 }
 
+console.log(range(2, 26));
 console.log(sum(range(1, 26)));
 console.log(sum(range(1, 100, 2)));
+console.log(range(10, 2, -3));
 console.log(sum(range(10, 2, -3)));
